4a65be77063bdd132f637309ce39c97f873465ba
rayhan <rayhangemming8@gmail.com>
2025-10-31 09:19:07 +0700

    feat: menambah materi baru

---
+++ pertemuan2/index.js
@@ -0,0 +1,95 @@
+let nama = "BAMBANG";
+
+
+// String
+let namaVar = "Ini Value String";
+let namaVar1 = 'String';
+// Number / Integer
+let namaVar2 = 100;
+// Boolean 
+let namaVar3 = true ;
+let namaVar4 = false ;
+// Null 
+let namaVar5 = null;
+// Undifined 
+let namaVar6 = undefined; 
+let namaVarr5;
+
+
+// console.log("Check Value: " +namaVar2);
+// console.log("Check Type Data: " + typeof namaVar2);
+
+
+
+function namaFunction() {
+    console.log("Si "+ nama + " Telah ngisi nama");
+    confirm("Oh kamu namanya adalah "+ nama +" kah?")
+    namaF2()
+}
+
+function namaF2(){
+    console.log("Ini Function 2 Berjalan");
+}
+
+// 3 OPSI 
+// LET - VAR - CONST
+
+// let alamat = prompt("Masukan Alamat Anda");
+
+let a = 90;
+let b = a;
+let c = b + a;
+let d = a + b * c ;
+
+var e = "Halo ";
+const f = "Nama ";
+let g = "Saya ";
+let h = "Bambang ";
+
+let result = e + f + g + h;
+
+console.log(result);
+// console.log((90*180)+90);
+
+
+function add(){
+    let a = prompt("Masukan Angka Pertama");
+    let b = prompt("Masukan Angka Kedua");
+    let result = Number(a) + Number(b)
+    console.log("Hasil dari "+ a +"+"+ b +" Adalah " + result);
+}
+
+function minus(){
+    let a = prompt("Masukan Angka Pertama");
+    let b = prompt("Masukan Angka Kedua");
+    let result = Number(a) - Number(b)
+    console.log("Hasil dari "+ a +"-"+ b +" Adalah " + result);
+}
+
+function multiply(){
+    let a = prompt("Masukan Angka Pertama");
+    let b = prompt("Masukan Angka Kedua");
+    let result = Number(a) * Number(b)
+    console.log("Hasil dari "+ a +"x"+ b +" Adalah " + result);
+}
+
+function devide(){
+    let a = prompt("Masukan Angka Pertama");
+    let b = prompt("Masukan Angka Kedua");
+    let result = Number(a) / Number(b)
+    console.log("Hasil dari "+ a +"/"+ b +" Adalah " + result);
+}
+
+function modulus(){
+    let a = prompt("Masukan Angka Pertama");
+    let b = prompt("Masukan Angka Kedua");
+    let result = Number(a) % Number(b)
+    console.log("Sisa Bagi dari "+ a +" Di Bagi "+ b +" Sisanya " + result);
+}
+
+function exponen(){
+    let a = prompt("Masukan Angka Pertama");
+    let b = prompt("Masukan Angka Kedua");
+    let result = Number(a) ** Number(b)
+    console.log("Hasil dari "+ a +" Pangkat "+ b +" Adalah " + result);
+}
