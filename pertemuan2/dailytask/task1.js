8c251d71dd4943e5f8528f99a79b0a180ee560a5
rayhan <rayhangemming8@gmail.com>
2025-10-31 15:51:33 +0700

    feat;tugas harian dan evaluasi harian udh selesai

---
+++ pertemuan2/dailytask/task1.js
@@ -0,0 +1,45 @@
+let String = "Contoh string" //string
+let integer = 50 //integer
+let boolean = true //boolean
+let nll = null; // null
+let undefined; //undefined
+
+const String1 = "Contoh string" //string
+const integer1 = 50 //integer
+const boolean1 = true //boolean
+const nll1 = null; // null
+// const undefined; //undefined
+
+var String2 = "Contoh string" //string
+var integer2 = 50 //integer
+var boolean2 = true //boolean
+var nll2 = null; // null
+var undefined2; //undefined
+
+let a = 5
+let b = 10
+console.log( a + b)// pertambahan
+console.log( a - b)// pengurangan
+console.log( a * b)// perkalian
+console.log( a / b)// pembagian
+console.log( a % b)// modulus
+console.log( a ** b)// perpangkatan
+
+let c = 20
+let d = 40
+
+console.log(a == b)// Equal
+console.log(a === b)// Strict Equal
+console.log(a != b)// Not Equal
+console.log(a !== b)// Strict Not Equal
+console.log(a > b)// lebih besar dari
+console.log(a < b)// lebih kecil dari
+console.log(a >= b)//Lebih besar atau sama dengan
+console.log(a <= b)//Lebih kecil atau sama dengan
+
+let e = 50
+let f = 60
+
+console.log(a > b  &&  f < e)//dandan
+console.log(a > b  ||  f < e)//Atau
+console.log(!true)//not
---
+++ pertemuan2/dailytask/task2.js
@@ -0,0 +1,11 @@
+let produk1 = "tas"
+let produk2 = "sendal" 
+let harga1 = 60000 
+let harga2 = 15000 
+
+console.log( produk1 + " Dengan Harga : " + harga1);
+console.log( produk2 + " Dengan Harga : " + harga2);
+
+let result = harga1 + harga2 
+
+console.log("Total Harga Ke dua produk : " + result);
