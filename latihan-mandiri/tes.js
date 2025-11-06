126e77ec5357c1fece0bf148aaacc3a70f5f3c18
rayhan <rayhangemming8@gmail.com>
2025-11-06 08:34:57 +0700

    feat: add on experiment

---
+++ latihan-mandiri/tes.js
@@ -0,0 +1,22 @@
+// array
+let nama = ["ijul","akhdan","muad","mutil"]
+console.log(nama);
+
+nama.push ("botak") // menambah data dari belakang
+console.log(nama);
+
+nama.unshift("ujang") // menambah data dari depan
+console.log(nama);
+
+nama.pop() //menghapus element terakhi
+console.log(nama);
+
+nama.shift() // menghapus element terdepan
+console.log(nama);
+
+console.log(nama.length);
+
+// Menyisipkan/Menghapus Elemen di Tengah
+//  `splice(index, jumlah, elemenBaru)`: hapus atau tambah elemen
+
+
